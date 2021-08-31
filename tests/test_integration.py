b689751cc73b9979b0865977d06a5ab00fd97f55
Frost Ming <mianghong@gmail.com>
2021-08-31 14:04:36 +0800

    Rewrite the plugin to support tox4

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,7 +1,6 @@
 import textwrap
 
 import py
-from tox.session import build_session, load_config, set_os_env_var, setup_reporter
 
 FIX_PROJECT = py.path.local(__file__).dirpath("fixture-project")
 
@@ -14,6 +13,9 @@ def setup_project(tmpdir, tox_config):
 
 
 def test_install_conditional_deps(tmpdir):
+    from tox.run import main
+    from tox import __version__ as TOX_VERSION
+
     test_config = textwrap.dedent(
         """
         [tox]
@@ -21,7 +23,7 @@ def test_install_conditional_deps(tmpdir):
         passenv = LD_PRELOAD
 
         [testenv]
-        sections =
+        groups =
             lint
         deps =
             django2: Django~=2.0
@@ -33,12 +35,10 @@ def test_install_conditional_deps(tmpdir):
     )
     setup_project(tmpdir, test_config)
     with tmpdir.as_cwd():
-        setup_reporter([])
-        config = load_config([])
-        config.logdir.ensure(dir=1)
-        with set_os_env_var(str("TOX_WORK_DIR"), config.toxworkdir):
-            session = build_session(config)
-            exit_code = session.runcommand()
-        assert exit_code == 0
+        main(["-c", str(tmpdir.join("tox.ini"))])
 
-    assert tmpdir.join(".tox/.package/demo-0.1.0.tar.gz").exists()
+    if TOX_VERSION[0] == "4":
+        package = tmpdir.join(".tox/4/.pkg/dist/demo-0.1.0.tar.gz")
+    else:
+        package = tmpdir.join(".tox/.package/demo-0.1.0.tar.gz")
+    assert package.exists()
